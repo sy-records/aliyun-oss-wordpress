47e1012b69a960f8ab9ceab49967c87f4ca4e89a
sy-records <52o@qq52o.cn>
2020-12-13 10:53:24 +0800

    Update sdk

--- sdk/vendor/composer/autoload_real.php
+++ sdk/vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitc01f83c61c7df694fdf929c88fea6382
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitc01f83c61c7df694fdf929c88fea6382', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitc01f83c61c7df694fdf929c88fea6382', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitc01f83c61c7df694fdf929c88fea6382::getInitializer($loader));
         } else {
---
+++ sdk/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e91742b194ff46741075d52a4ea0fc1148659407',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e91742b194ff46741075d52a4ea0fc1148659407',
+    ),
+    'aliyuncs/oss-sdk-php' => 
+    array (
+      'pretty_version' => 'v2.4.1',
+      'version' => '2.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '492866331b7bafaac09506cf42f351b7e9e63766',
+    ),
+  ),
+);
