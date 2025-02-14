04023f72eb7b96811e67760c902131fec4a7d14e
Luffy <52o@qq52o.cn>
2025-02-14 14:55:03 +0800

    Optimize uninstall code (#49)

--- uninstall.php
+++ uninstall.php
@@ -4,12 +4,5 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
-$options             = get_option('oss_options', true);
-$upload_url_path     = get_option('upload_url_path');
-$oss_upload_url_path = esc_attr($options['upload_url_path']);
-
-if ($upload_url_path == $oss_upload_url_path) {
-    update_option('upload_url_path', '');
-}
-
+update_option('upload_url_path', '');
 delete_option('oss_options');
